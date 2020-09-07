14bdcbeab861ce1130e08e2d586cccbf5e6e8a8d
Kaushal Patel <55987371+mrkaushalpatel@users.noreply.github.com>
2020-09-07 17:29:35 +0530

    middleware

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',               #Added Middleware
 ]
 
 ROOT_URLCONF = 'iCoder.urls'
