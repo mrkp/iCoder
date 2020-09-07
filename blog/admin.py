4a3455d05b7caa3f2866b07b815a6f6b119c971e
Kaushal Patel <55987371+mrkaushalpatel@users.noreply.github.com>
2020-09-07 12:07:20 +0530

    TinyMce Created

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from .models import Post,BlogComment
 
 # Register your models here.
 
-admin.site.register((Post, BlogComment))
+admin.site.register((BlogComment))
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('js/tinyInject.js',)
